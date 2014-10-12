21ca8026a20327461ec931c9bd761479b0043b24
Aflynch <aflynch@gmail.com>
2014-10-12 13:31:04 -0400

    Edited database, added view, builder for view.
    Added Tables : Games  and  Platforms to DannysList database. Some test
    data was added into the tables.
    
    Added resultsviewbuilder.java
    This will act a controller for resultsview.jsp
    
    Added resultsview.jsp
    This will show the results from searches.

--- DannysList/WebContent/createdannyslist.sql
+++ DannysList/WebContent/createdannyslist.sql
@@ -9,3 +9,34 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (username, password) VALUES ("testuser", "testpassword");
+
+/*
+ *AFL
+ *Adding a new tables for testing view builder.
+ *Questions
+ *Email		: Aflynch@gmail.com
+ *Phone 	: 678 900 6694
+ */	 
+CREATE TABLE Platforms(
+	platform_id INT NOT NULL AUTO_INCREMENT,
+	platform VARCHAR(30) NOT NULL,
+	PRIMARY KEY(platform_id)
+);
+	
+CREATE TABLE Games(
+	name VARCHAR(30) PRIMARY KEY UNIQUE,
+	platform_id int NOT NULL,
+	genres VARCHAR(30) NOT NULL,
+	year INT NOT NULL,
+	cost DOUBLE NOT NULL,
+	FOREIGN KEY (platform_id) REFERENCES Platforms(platform_id)
+);
+
+INSERT INTO Platforms (platform) VALUES ("N46");
+INSERT INTO Platforms (platform) VALUES ("Playstation");
+
+INSERT INTO games ( name, platform_id, genres, year, cost) VALUES ("Dr. Im", 1 , "Puzzle", 1981, 65000.00);
+/*
+ *AFL
+ *End of changes for testing view builder.
+ */
